4b3709823b9aef702738489a9af18812d38f76ca
Rafael Chapine <rchapine@gmail.com>
2021-05-25 21:50:44 -0400

    dellpc brach

--- src/sections/key-feature.js
+++ src/sections/key-feature.js
@@ -44,7 +44,28 @@ const data = [
 ];
 
 export default function KeyFeature() {
-  return <section id="feature"></section>;
+  return (
+  <section sx={{ variant: 'section.keyFeature'}} id="feature">
+    <Container>
+      <SectionHeader
+        slogan="What the function"
+        title="Meet the feature of our product"
+      />
+      <Grid sx={styles.grid}>
+        {data.map((item) => (
+          <FeatureCardColumn
+             key={item.id}
+             src={item.imgSrc}
+             alt={item.altText}
+             title={item.title}
+             text={item.text}
+          />
+        ))}
+      </Grid>
+    </Container>
+
+  </section>
+  );
 }
 
 const styles = {
